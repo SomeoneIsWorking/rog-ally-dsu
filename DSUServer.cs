548c2a72b2a59e12eee230f693521ef7bc4d9381
Barış Hamil <barishamil90@gmail.com>
2024-08-31 12:58:32 +0300

    Fixed SendControllerData issues

--- DSUServer.cs
+++ DSUServer.cs
@@ -118,67 +118,65 @@ class DSUServer
     private unsafe void SendControllerData()
     {
         if (!isControllerConnected)
-            return;
-
-        var state = xboxController.GetState();
+            return; var state = xboxController.GetState();
         var gamepad = state.Gamepad;
 
-        byte[] response = new byte[100];
-        Array.Copy(Encoding.ASCII.GetBytes("DSUS"), 0, response, 0, 4);
-        Array.Copy(BitConverter.GetBytes((ushort)ProtocolVersion), 0, response, 4, 2);
-        Array.Copy(BitConverter.GetBytes((ushort)80), 0, response, 6, 2);
-        Array.Copy(BitConverter.GetBytes((uint)0), 0, response, 8, 4);
-        Array.Copy(BitConverter.GetBytes((uint)1), 0, response, 12, 4);
-        Array.Copy(BitConverter.GetBytes((uint)0x100002), 0, response, 16, 4);
+        using var memoryStream = new MemoryStream();
+        using var writer = new BinaryWriter(memoryStream);
 
         // Example controller data
-        response[20] = 0; // Slot
-        response[21] = 2; // Slot state (connected)
-        response[22] = 2; // Device model (full gyro)
-        response[23] = 1; // Connection type (USB)
-        Array.Copy(new byte[6], 0, response, 24, 6); // MAC address
-        response[30] = 4; // Battery status (High)
-        response[31] = 0; // Zero byte
+        writer.Write((byte)0); // Slot
+        writer.Write((byte)2); // Slot state (connected)
+        writer.Write((byte)2); // Device model (full gyro)
+        writer.Write((byte)1); // Connection type (USB)
+        writer.Write(new byte[6], 0, 6); // MAC address
+        writer.Write((byte)4); // Battery status (High)
 
         // Controller state data
-        response[32] = 1; // Is controller connected
-        Array.Copy(BitConverter.GetBytes((uint)1), 0, response, 36, 4); // Packet number
-        response[40] = (byte)((gamepad.Buttons & GamepadButtonFlags.DPadLeft) != 0 ? 1 : 0); // D-Pad Left
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadDown) != 0 ? 2 : 0); // D-Pad Down
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadRight) != 0 ? 4 : 0); // D-Pad Right
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadUp) != 0 ? 8 : 0); // D-Pad Up
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.Start) != 0 ? 16 : 0); // Start
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.RightThumb) != 0 ? 32 : 0); // R3
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.LeftThumb) != 0 ? 64 : 0); // L3
-        response[40] |= (byte)((gamepad.Buttons & GamepadButtonFlags.Back) != 0 ? 128 : 0); // Back
+        writer.Write((byte)1); // Is controller connected
+        writer.Write(BitConverter.GetBytes((uint)1)); // Packet number
+        byte buttons = 0;
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadLeft) != 0 ? 1 : 0); // D-Pad Left
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadDown) != 0 ? 2 : 0); // D-Pad Down
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadRight) != 0 ? 4 : 0); // D-Pad Right
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.DPadUp) != 0 ? 8 : 0); // D-Pad Up
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.Start) != 0 ? 16 : 0); // Start
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.RightThumb) != 0 ? 32 : 0); // R3
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.LeftThumb) != 0 ? 64 : 0); // L3
+        buttons |= (byte)((gamepad.Buttons & GamepadButtonFlags.Back) != 0 ? 128 : 0); // Back
+        
+        writer.Write(buttons);
 
-        response[41] = (byte)((gamepad.Buttons & GamepadButtonFlags.Y) != 0 ? 1 : 0); // Y
-        response[41] |= (byte)((gamepad.Buttons & GamepadButtonFlags.B) != 0 ? 2 : 0); // B
-        response[41] |= (byte)((gamepad.Buttons & GamepadButtonFlags.A) != 0 ? 4 : 0); // A
-        response[41] |= (byte)((gamepad.Buttons & GamepadButtonFlags.X) != 0 ? 8 : 0); // X
-        response[41] |= (byte)((gamepad.Buttons & GamepadButtonFlags.RightShoulder) != 0 ? 16 : 0); // R1
-        response[41] |= (byte)((gamepad.Buttons & GamepadButtonFlags.LeftShoulder) != 0 ? 32 : 0); // L1
-        response[41] |= (byte)((gamepad.RightTrigger > 0 ? 64 : 0)); // R2
-        response[41] |= (byte)((gamepad.LeftTrigger > 0 ? 128 : 0)); // L2
+        byte buttons2 = 0;
+        buttons2 |= (byte)((gamepad.Buttons & GamepadButtonFlags.Y) != 0 ? 1 : 0); // Y
+        buttons2 |= (byte)((gamepad.Buttons & GamepadButtonFlags.B) != 0 ? 2 : 0); // B
+        buttons2 |= (byte)((gamepad.Buttons & GamepadButtonFlags.A) != 0 ? 4 : 0); // A
+        buttons2 |= (byte)((gamepad.Buttons & GamepadButtonFlags.X) != 0 ? 8 : 0); // X
+        buttons2 |= (byte)((gamepad.Buttons & GamepadButtonFlags.RightShoulder) != 0 ? 16 : 0); // R1
+        buttons2 |= (byte)((gamepad.Buttons & GamepadButtonFlags.LeftShoulder) != 0 ? 32 : 0); // L1
+        buttons2 |= (byte)(gamepad.RightTrigger > 0 ? 64 : 0); // R2
+        buttons2 |= (byte)(gamepad.LeftTrigger > 0 ? 128 : 0); // L2
+        
+        writer.Write(buttons2);
 
-        response[42] = 0; // HOME button (not available on Xbox controller)
-        response[43] = 0; // Touch button (not available on Xbox controller)
-        response[44] = (byte)(gamepad.LeftThumbX / 256 + 128); // Left stick X
-        response[45] = (byte)(gamepad.LeftThumbY / 256 + 128); // Left stick Y
-        response[46] = (byte)(gamepad.RightThumbX / 256 + 128); // Right stick X
-        response[47] = (byte)(gamepad.RightThumbY / 256 + 128); // Right stick Y
-        response[48] = 0; // Analog D-Pad Left
-        response[49] = 0; // Analog D-Pad Down
-        response[50] = 0; // Analog D-Pad Right
-        response[51] = 0; // Analog D-Pad Up
-        response[52] = 0; // Analog Y
-        response[53] = 0; // Analog B
-        response[54] = 0; // Analog A
-        response[55] = 0; // Analog X
-        response[56] = 0; // Analog R1
-        response[57] = 0; // Analog L1
-        response[58] = (byte)gamepad.RightTrigger; // Analog R2
-        response[59] = (byte)gamepad.LeftTrigger; // Analog L2
+        writer.Write((byte)0); // HOME button (not available on Xbox controller)
+        writer.Write((byte)0); // Touch button (not available on Xbox controller)
+        writer.Write((byte)(gamepad.LeftThumbX / 256 + 128)); // Left stick X
+        writer.Write((byte)(gamepad.LeftThumbY / 256 + 128)); // Left stick Y
+        writer.Write((byte)(gamepad.RightThumbX / 256 + 128)); // Right stick X
+        writer.Write((byte)(gamepad.RightThumbY / 256 + 128)); // Right stick Y
+        writer.Write((byte)0); // Analog D-Pad Left
+        writer.Write((byte)0); // Analog D-Pad Down
+        writer.Write((byte)0); // Analog D-Pad Right
+        writer.Write((byte)0); // Analog D-Pad Up
+        writer.Write((byte)0); // Analog Y
+        writer.Write((byte)0); // Analog B
+        writer.Write((byte)0); // Analog A
+        writer.Write((byte)0); // Analog X
+        writer.Write((byte)0); // Analog R1
+        writer.Write((byte)0); // Analog L1
+        writer.Write((byte)gamepad.RightTrigger); // Analog R2
+        writer.Write((byte)gamepad.LeftTrigger); // Analog L2
 
         // Read accelerometer and gyroscope data from SDL
         float accelX = 0, accelY = 0, accelZ = 0;
@@ -203,13 +201,16 @@ class DSUServer
             }
         }
 
-        Array.Copy(BitConverter.GetBytes((ulong)0), 0, response, 72, 8); // Motion data timestamp
-        Array.Copy(BitConverter.GetBytes(accelX), 0, response, 80, 4); // Accelerometer X
-        Array.Copy(BitConverter.GetBytes(accelY), 0, response, 84, 4); // Accelerometer Y
-        Array.Copy(BitConverter.GetBytes(accelZ), 0, response, 88, 4); // Accelerometer Z
-        Array.Copy(BitConverter.GetBytes(gyroPitch), 0, response, 92, 4); // Gyroscope pitch
-        Array.Copy(BitConverter.GetBytes(gyroYaw), 0, response, 96, 4); // Gyroscope yaw
-        Array.Copy(BitConverter.GetBytes(gyroRoll), 0, response, 100, 4); // Gyroscope roll
+        writer.Write(BitConverter.GetBytes((ulong)0)); // Motion data timestamp
+        writer.Write(BitConverter.GetBytes(accelX)); // Accelerometer X
+        writer.Write(BitConverter.GetBytes(accelY)); // Accelerometer Y
+        writer.Write(BitConverter.GetBytes(accelZ)); // Accelerometer Z
+        writer.Write(BitConverter.GetBytes(gyroPitch)); // Gyroscope pitch
+        writer.Write(BitConverter.GetBytes(gyroYaw)); // Gyroscope yaw
+        writer.Write(BitConverter.GetBytes(gyroRoll)); // Gyroscope roll
+
+        writer.Flush();
+        byte[] response = memoryStream.ToArray();
 
         udpClient.Send(response, response.Length, remoteEndPoint);
     }
